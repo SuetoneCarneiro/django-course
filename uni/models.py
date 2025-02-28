4bd6b5a49843685ec411dee9ef24bf200dbc182a
Suetone Carneiro <suetoneneto12@gmail.com>
2025-02-27 22:22:07 -0300

    feat: primeira model criada como exemplo

--- uni/models.py
+++ uni/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Usuario(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(unique=True, null=False)
+    nome_completo = models.CharField(
+        max_length=255, verbose_name='Nome Completo')
+    endereco = models.CharField(max_length=255, verbose_name='Endereço')
+    telefone = models.CharField(max_length=22, verbose_name='Telefone')
+
+    def __str__(self):
+        return f'{self.nome_completo} - {self.email}'
